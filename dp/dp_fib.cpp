e547a29498ced8c8b51fe3bd654630d79142dee1
sahil dantani <sahildutt146@gmail.com>
2023-09-02 15:36:07 +0530

    dp

---
+++ dp/dp_fib.cpp
@@ -0,0 +1,34 @@
+/*
+find fib(n)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+10;
+int dp[N];
+
+int fib(int n){
+
+    // TOP-DOWN APPROACH
+    if(n==0)return 0;
+    if(n==1)return 1;
+    if(dp[n]!= -1)return dp[n];//this call memoisation
+    return dp[n]=fib(n-1)+fib(n-2);
+}
+
+int main(){
+    memset(dp,-1,sizeof(dp));
+    int n;
+    cin>>n;
+
+    // BOTTOM_UP APPROACH
+    /*
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<n;i++){
+     dp[i]=dp[i-1]+dp[i-2];
+    }
+    */
+    cout<<fib(n);
+}
